b41a8a5df120f26b3a792c3355da5571633a64ad
Oleg Virovkin <oleg.virovkin@gmail.com>
2019-02-03 23:39:32 +0000

    added temp screenshot path

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -20,8 +20,9 @@ module.exports = {
     "default": {
       "screenshots": {
         "enabled": true, 
+        "path": TEST_RESULTS_PATH,
         "on_failure" : true,
-        "path": TEST_RESULTS_PATH
+        "on_error": true,
       },
       "globals": {
         "waitForConditionTimeout": 5000
@@ -48,27 +49,4 @@ module.exports = {
   }
 }
 
-function padLeft (count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
-  return count < 10 ? '0' + count : count.toString();
-}
-
-var FILECOUNT = 0; // "global" screenshot file count
-/**
- * The default is to save screenshots to the root of your project even though
- * there is a screenshots path in the config object above! ... so we need a
- * function that returns the correct path for storing our screenshots.
- * While we're at it, we are adding some meta-data to the filename, specifically
- * the Platform/Browser where the test was run and the test (file) name.
- */
-function imgpath (browser) {
-  var a = browser.options.desiredCapabilities;
-  var meta = [a.platform];
-  meta.push(a.browserName ? a.browserName : 'any');
-  meta.push(a.version ? a.version : 'any');
-  meta.push(a.name);
-  var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
-  return TEST_RESULTS_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
-}
-
-module.exports.imgpath = imgpath;
 module.exports.TEST_RESULTS_PATH = TEST_RESULTS_PATH;
